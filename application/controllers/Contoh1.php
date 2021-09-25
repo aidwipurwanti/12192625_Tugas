7465a8334ca26a140ca269f5d181544f70a068e8
aidwipurwanti <aidwi29091999@gmail.com>
2021-09-25 17:07:19 +0700

    menambahkan framework

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Control extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Ai Dwi Purwanti
+    Saya tinggal di daerah Cibeureum
+    olah raga yang saya sukai adalah Bulutangkis";
+	}
+}
